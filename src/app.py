4716be376f5e68aeeba3525cba265f6ac695e149
nielad <nielad@gmail.com>
2024-04-04 21:12:19 -0400

    created new dirs

---
+++ src/app.py
@@ -0,0 +1,174 @@
+import streamlit as st 
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import ast
+import math
+
+df_2024 = pd.read_csv("other_data/presidential predict data 2024.csv")
+df_train = pd.read_csv("other_data/presidential election training data 2000-2020.csv")
+df_paths_to_victory = pd.read_csv("other_data/paths_to_victory.csv")
+
+df_train.set_index('state_year', inplace=True)
+df_2024.set_index('state_year', inplace=True)
+state_poll_dict = df_2024['dem_poll_advantage'].to_dict()
+state_poll_dict = {key.split("_")[0]: value for key, value in state_poll_dict.items()}
+
+st.write("""
+# Electify
+## 2024 Election Forecasting
+
+""")
+
+
+# The slider options variable is a list. The state_spread float needs to be inserted
+# a list of integers between -9 and 9, then cast as a string in order to 
+# prefix the two sides with "B+" and "T+"
+	
+
+def create_state_slider_options(integers_list, state_spread):
+	# cast the integers (and float) as a string and replace the minus signs with plus signs 
+	# and the corresponding candidate letter
+	strings_list = [("B+" + str(num).split("-")[1]) if num < 0 else ("T+" + str(num)) for num in integers_list]
+	
+	if state_spread > 0:
+		strings_list[9] = "0"
+	else:
+		strings_list[10] = "0"
+	
+	
+	strings_list.insert(0, "Biden")
+	strings_list.append("Trump")
+	return strings_list
+	
+def create_state_slider_int_list(state_spread, opts):
+	# creates slider range
+	integers_list = []
+	if opts:
+		integers_list = list(range(-9, 10))
+	else:
+		integers_list = list(range(-10,11))
+	
+	
+	integers_list.append(state_spread)
+	integers_list = sorted(integers_list)
+	return integers_list
+
+
+	
+def create_slider_params(state):
+	state_spread = state_poll_dict[state]
+	
+	val = ''
+	# val will represent the default value when the page is rendered, it needs to correspond to an element in the 
+	# integers_list that initializes options...
+	if state_spread < 0:
+		# removes the negative sign for Trump
+		val = 'T+' + str(state_spread).split("-")[1]
+	elif state_spread > 0:
+		val = 'B+' + str(state_spread)
+	
+	# ...which is why we flip the state_spread sign,
+	state_spread *= -1
+	int_list = create_state_slider_int_list(state_spread, opts=True)
+	
+	options = create_state_slider_options(int_list, state_spread)
+	
+	return st.sidebar.select_slider(state, options = options, value = val)
+
+
+	# 
+def create_polling_spread_input():
+	arizona, georgia, michigan, nevada, pennsylvania, wisconsin = [create_slider_params(state) for state in state_poll_dict.keys()]
+	
+	# input needs to be converted back to floats before running through model
+	return arizona, georgia, michigan, nevada, pennsylvania, wisconsin 
+
+def create_input_mapping(state):
+	state_spread = state_poll_dict[state]
+	state_spread *= -1
+	keys = create_state_slider_int_list(state_spread, opts=True)
+	keys = create_state_slider_options(keys, state_spread)
+	
+	
+	int_list = create_state_slider_int_list(state_spread, opts=False)
+	float_list = [float(x) for x in int_list]
+	mapping = dict(zip(keys, float_list))
+	return mapping
+
+def clean_user_input(state_poll_dict, user_input_raw_list):
+	x = 0
+	user_input_clean_list = []
+	for state in state_poll_dict.keys():
+		# don't forget to flip the sign again. Biden's sign is positive if he is in the lead, negative if losing
+		state_map = create_input_mapping(state)
+		user_input_raw = user_input_raw_list[x]
+		
+		user_input_clean = state_map[user_input_raw]
+		x += 1
+		user_input_clean_list.append(user_input_clean)
+	return user_input_clean_list
+
+def find_total_prob_of_winning(paths, probs):
+
+    outcomes_probabilities = []
+    
+    for outcome in paths:
+        probability = 1
+        for state, prob in probs.items():
+            if state in outcome:
+                probability *= prob
+            else:
+                probability *= (1 - prob)
+        outcomes_probabilities.append(probability)
+    
+    return sum(outcomes_probabilities)
+
+def to_percent_up(x):
+    if isinstance(x, float):
+        return f"{math.ceil(x * 100)}%"
+    return x
+	
+@st.cache_resource
+def load_model():
+	rfc = RandomForestClassifier(n_estimators = 100)
+	return rfc
+	
+st.sidebar.header('Polling Spread')
+arizona, georgia, michigan, nevada, pennsylvania, wisconsin = create_polling_spread_input()
+user_input_raw_list = [arizona, georgia, michigan, nevada, pennsylvania, wisconsin]
+
+
+user_input_clean_list =clean_user_input(state_poll_dict, user_input_raw_list)
+dem_poll_advantage = [poll * -1 for poll in user_input_clean_list]
+df_2024['dem_poll_advantage'] = dem_poll_advantage
+
+
+X = df_train[df_train.columns[0:8]]
+Y = df_train.winner
+
+rfc = load_model()
+#rfc = RandomForestClassifier(n_estimators = 100)
+rfc.fit(X, Y)
+
+predict_prob= (rfc.predict_proba(df_2024[df_2024.columns[0:8]]))
+df_2024_predictions = pd.DataFrame(predict_prob, columns=['biden', 'trump'], index =df_2024.index)
+df_2024_predictions.index = df_2024_predictions.index.str.replace('_2024', '')
+df_2024_predictions.index.name = ''
+
+st.write(""" ### Probability of winning each state""")
+
+df_2024_predictions_display = df_2024_predictions.applymap(to_percent_up)
+
+st.dataframe(df_2024_predictions_display)
+
+
+trump_paths = ast.literal_eval(df_paths_to_victory[df_paths_to_victory['candidate'] == 'Trump']['paths_to_victory'].iloc[0])
+trump_probs = df_2024_predictions['trump']
+biden_paths = ast.literal_eval(df_paths_to_victory[df_paths_to_victory['candidate'] == 'Biden']['paths_to_victory'].iloc[0])
+biden_probs = df_2024_predictions['biden']
+
+st.write(""" ### Probability of winning the presidential election""")
+
+st.write("Trump probability of winning: " + str(to_percent_up(find_total_prob_of_winning(trump_paths, trump_probs))))
+st.write("Biden probability of winning: " + str(to_percent_up(find_total_prob_of_winning(biden_paths, biden_probs))))
---
+++ src/get_current_polls.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import os
+import requests
+import json
+from datetime import datetime
+import shutil
+
+print(os.getcwd())
+current_date = datetime.now().strftime('%Y-%m-%d')
+state = 'North Carolina'
+
+battleground_states = {'Wisconsin' : 'https://www.realclearpolitics.com/poll/race/7398/polling_data.json', 
+                       'Pennsylvania' :'https://www.realclearpolitics.com/poll/race/7892/polling_data.json',
+                       'Nevada' : 'https://www.realclearpolitics.com/poll/race/7826/polling_data.json',                 
+                       'Georgia' : 'https://www.realclearpolitics.com/poll/race/7747/polling_data.json',
+                       'Arizona' : 'https://www.realclearpolitics.com/poll/race/7907/polling_data.json',
+                       'Michigan' : 'https://www.realclearpolitics.com/poll/race/7953/polling_data.json'}
+
+def download_rcp_files():
+    if not os.path.exists('rcp_data'):
+        os.makedirs('rcp_data')
+    move_old_files()
+    # send http request to the RCP json file
+    for state, url in battleground_states.items():
+        send_http_request(url, state)
+def move_old_files():
+    directory = 'rcp_data'
+    old_directory = os.path.join(directory, 'old_polling_data')
+    if not os.path.exists(old_directory):
+        os.makedirs(old_directory)
+        
+    # List all files in the directory
+    files = os.listdir(directory)
+
+    # Iterate over each file
+    for file in files:
+        # Check if the file is a .json file
+        if file.endswith('.json'):
+            # Construct the file path
+            file_path = os.path.join(directory, file)
+            old_file_path = os.path.join(old_directory, file)
+    
+            # Move the file to the 'old_polling_data' subdirectory
+            shutil.move(file_path, old_file_path)
+            print(f"Moved file: {file_path}")
+            
+def send_http_request(url, state):
+    response = requests.get(url)
+    # if the response is OK, write the file to the polling data directory
+    if response.status_code == 200:
+        write_file(state, response)
+    else:
+        print(f"Failed to download {state} polling data")
+
+def write_file(state, response):
+    state_title = f'{state}_polling_data_{current_date}.json'
+    filename = os.path.join('rcp_data', state_title)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+        print(f"Downloaded {state} polling data")
+
+
+def extract_poll_data():
+
+
+    # Define the directory where JSON files are stored
+    directory = 'rcp_data'
+    poll_list = []
+    
+    # Iterate over each file in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            # Construct the full path to the file
+            filepath = os.path.join(directory, filename)
+            
+            # Open the file and process its contents and create the list of dictionaries that contains the data of each file
+            poll_dict = open_file(filepath, filename)
+            poll_list.append(poll_dict)
+    return poll_list
+
+def open_file(filepath, filename):
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+        
+        # Process the data as needed
+        print(f"Processing {filename}:")
+        poll_dict = extract_poll_data_helper(data)
+        
+        # Extract the state name from the filename
+        state_name = filename.split('_')[0]
+        poll_dict['state'] = state_name
+
+        poll_dict['date'] = current_date
+        return poll_dict
+        
+def extract_poll_data_helper(data):
+# Access the "poll" object
+    poll_data = [poll for poll in data['poll'] if poll['type'] == 'rcp_average']
+          
+    # Extracting the required values
+    poll_id = poll_data[0]['id']
+    poll_date = poll_data[0]['date']
+    second_date = poll_date.split("-")[1].strip()
+    trump_value = next(candidate['value'] for candidate in poll_data[0]['candidate'] if candidate['name'] == 'Trump')
+    biden_value = next(candidate['value'] for candidate in poll_data[0]['candidate'] if candidate['name'] == 'Biden')
+    spread_name = poll_data[0]['spread']['name']
+    spread_value = poll_data[0]['spread']['value']
+    poll_dict = {'id' : poll_id, 
+                 'last_poll_date' : second_date,
+                 'trump' : trump_value,
+                 'biden' : biden_value,
+                 'spread_name' : spread_name,
+                 'spread_value' : spread_value.split('+')[1] }
+    print(poll_dict)
+
+    return poll_dict
+
+
+
+download_rcp_files()
+rcp_data = extract_poll_data()
+
+df = pd.DataFrame(rcp_data)
+df['dem_poll_advantage'] = df.apply(lambda row: -float(row['spread_value']) if row['spread_name'] == 'Trump' else row['spread_value'], axis=1)
+
+df.to_csv("other_data/daily_state_polling_averages.csv", index = False)
+print("Dataframe read to other_data/daily_state_polling_averages.csv")
+print(os.getcwd())
