06812d07746d5ea9bae0546f087052c892616c02
nielad <nielad@gmail.com>
2024-04-10 14:53:08 -0700

    Update app.py
    testing modified github actions yaml entrypoint for docker

--- src/app.py
+++ src/app.py
@@ -14,7 +14,8 @@ df_2024.set_index('state_year', inplace=True)
 state_poll_dict = df_2024['dem_poll_advantage'].to_dict()
 state_poll_dict = {key.split("_")[0]: value for key, value in state_poll_dict.items()}
 
-st.write("Succesfully setup CI/CD *****")
+st.write("Succesfully setup CI/CD ***********")
+st.write("all your base are belong to us")
 st.write("""
 # Electify
 ## 2024 Election Forecasting
