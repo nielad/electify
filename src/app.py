1cb0b8937337d6151cfe060f73fffd80363459ea
nielad <nielad@gmail.com>
2024-04-10 21:25:54 -0700

    update: add icon and page title
    deleted unused dockerfile

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 import ast
 import math
 
+st.set_page_config(page_title="Electify", page_icon="images/favicon.webp")
+
 df_2024 = pd.read_csv("other_data/presidential predict data 2024.csv")
 df_train = pd.read_csv("other_data/presidential election training data 2000-2020.csv")
 df_paths_to_victory = pd.read_csv("other_data/paths_to_victory.csv")
