b3cdad63d566d6757b262ec9414035dd47344d8f
nielad <nielad@gmail.com>
2024-04-11 11:06:24 -0700

    Update add_cron_job.sh
    fix cron path to python

--- src/add_cron_job.sh
+++ src/add_cron_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add the cron job to the crontab
-echo "* * * * * python /src/hello.py >> /var/log/hello.log 2>&1" | crontab -
+echo "* * * * * /usr/local/bin/python /src/hello.py >> /var/log/hello.log 2>&1" | crontab -
