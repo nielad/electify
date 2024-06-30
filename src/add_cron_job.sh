c3d7b2bf38109bca13882aec3bab5780c8fe9e5a
nielad <nielad@gmail.com>
2024-06-30 15:01:53 -0700

    test cronjob

--- src/add_cron_job.sh
+++ src/add_cron_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Add the cron job to the crontab
-(echo "* */23 * * * /usr/local/bin/python /src/get_current_polls.py >> /var/log/get_current_polls.log 2>&1"
+(echo "* * * * * /usr/local/bin/python /src/get_current_polls.py >> /var/log/get_current_polls.log 2>&1"
  echo "* * * * * /usr/local/bin/python /src/merge_dataframes.py >> /var/log/merge_dataframes.log 2>&1") | crontab -
