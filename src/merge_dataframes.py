59f7796a9957162d1576c4e5d0ad8a08725bdaae
nielad <nielad@gmail.com>
2024-06-30 15:28:46 -0700

    Update merge_dataframes.py

--- src/merge_dataframes.py
+++ src/merge_dataframes.py
@@ -10,4 +10,4 @@ df_polls_2024 = df_polls_2024[['state_year', 'dem_poll_advantage']]
 df_2024 = pd.merge(df_polls_2024, df_2024, how = 'outer')
 df_2024.dropna(inplace=True)
 df_2024.to_csv("/src/other_data/presidential predict data 2024.csv", index = False)
-print("merge dataframes complete")
+print("merge dataframes complete!")
